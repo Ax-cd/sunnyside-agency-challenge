4976e7746aa93a049053f8a715e9555201cb238b
Ax-cd <axcd.coder@gmail.com>
2022-06-18 14:06:02 +0200

    add menu-corner

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.querySelector(".toggle");
   const menu = document.querySelector(".menu");
+  const corner = document.querySelector(".menu-corner");
   const h1 = document.querySelector(".h1");
   const arrow = document.querySelector(".arrow");
 
   function toggleMenu() {
     toggleBtn.addEventListener("click", function (e) {
       menu.classList.toggle("hide-menu");
+      corner.classList.toggle("hide-menu");
       if (menu.classList.contains("hide-menu")) {
         //hide menu
         h1.style.opacity = 1;
